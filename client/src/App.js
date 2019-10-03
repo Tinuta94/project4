bc07a59e3a4a91adf7b6209da3aa2442a29277f4
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-10-03 12:49:18 -0400

    adeed a component home

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Link, Route} from "react-router-dom"
+import {Link, Route, Switch} from "react-router-dom"
 
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 // import Typography from '@material-ui/core/Typography';
 // import Box from '@material-ui/core/Box';
-
+import Home from './components/Home.js'
 
 
 function App() {
   return (
     <div className="App">
-      <AppBar position="static">
+      <AppBar position="static" style={{backgroundColor: "pink"}}>
         <Tabs value="" onChange="" aria-label="simple tabs example" >
         
-        <Tab label="Home" />
-          <Link><Tab label="About Us" /></Link>
+        <Link><Tab label="Home" /></Link>
+
+          <Link><Tab label="" /></Link>
+          <Link><Tab label=" Locations" /></Link>
           <Tab label="" />
-          <Tab label="Our Locations" />
+         <Link> <Tab label="Gallery" /></Link>
           <Tab label="" />
-          <Tab label="Gallery" />
-          <Tab label="" />
-          <Tab label="Our Services" />
+         <Link> <Tab label="Services" /></Link>
+         <Tab label="" />
+         <Link> <Tab label="Contact" /></Link>
           
         </Tabs>
       </AppBar>
-      {/* <TabPanel value="" index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value="" index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value="" index={2}>
-        Item Three
-      </TabPanel> */}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        {/* <Route exact path="/locations" component={Locations}/> */}
+      </Switch>
     </div>
   );
 }
---
+++ client/src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+ class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Un Petit Coin de Paradis</h1>
+                <div class="gif">
+                
+                </div>
+            </div>
+        )
+    }
+}
+export default Home;
