e6d4f68bca27e3216196c8d434a868d9a6770063
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-10-05 17:16:02 -0400

    added contact to the comp

---
+++ client/src/components/Contact.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+ class Contact extends Component {
+    render() {
+        return (
+            <div>
+            <div class="anim">
+           
+                <h3 class="contact">Please contact us to make an appointment or schedule a free consultation with our specialists.</h3>
+
+                 
+                <br></br>           
+                  <h3>Email: paradis@gmail.com</h3>
+               
+                  <h3>Phone: (474)-767-5854</h3>
+            </div>
+            </div>
+            
+        )
+    }
+}
+export default Contact;
