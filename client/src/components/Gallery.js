e235e662dd46a4af848703036c1df9609833f833
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-10-05 15:39:32 -0400

    created a new component gallery

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -7,14 +7,28 @@ import 'react-awesome-slider/dist/styles.css';
         return (
             <div>
                 
-  <AwesomeSlider>
+  <AwesomeSlider className="clasa">
      
-    <div data-src="http://www.joseeber.com/wp-content/uploads/2015/11/LALOGE_HIRES_2-e1454539915305.jpg" />
-    
+    <div class="foto" data-src="https://wallup.net/wp-content/uploads/2016/02/20/293702-Xenia_Deli-model-brunette-face-women-simple_background-748x468.jpg" />
+    <div data-src="https://cdn.shopify.com/s/files/1/0066/0052/articles/Top_Knots_1400x.jpg?v=1548453483"/>
+    <div data-src="https://www.prideofhair.com/wp-content/uploads/2017/05/Best-Haircuts-for-Women-2019.jpg"/>
+    <div data-src="https://f.herstylecode.com/2016/11/best-easy-long-hairstyles.jpg"/>
+    <div data-src="http://pricheska-strizhka.ru/wp-content/uploads/2018/06/Best-Long-Hairstyles-and-Haircuts.jpg"/>
+    <div data-src="https://pastelesde.com/wp-content/uploads/2018/10/Long-Hairstyles-Archives-Hairstyles-2019-New-Haircuts-and-Hair-Colors.jpg"/>
+    <div class="foto" data-src="https://fustany.com/images/en/content/header_image_header_image_Article-Main-Fustany-hair-Braid-hairstyles-EN.jpg"/>
+    <div data-src="https://thegoddess.com/wp-content/uploads/2017/02/37-Easy-Hairstyles-for-Work-Featured-Image.jpg"/>
+    <div data-src="https://www.menshairstylesnow.com/wp-content/uploads/2017/06/Cool-Haircut-Designs-For-Men.jpg"/>
+    <div data-src="https://i.ytimg.com/vi/o4J1RZOj6X4/maxresdefault.jpg"/>
+    <div data-src="https://beautyhealthtips.in/wp-content/uploads/2019/02/Hair-style-according-to-face-shape-male.jpg"/>
+    <div data-src="https://ringmyfashion.com/wp-content/uploads/2019/02/Mid-Fade-Haircuts-For-Men.jpg"/>
+    <div data-src="https://www.hudsonvalleycelticfestival.com/image/i.ytimg.com/vi/Y2t2Oem32tA/maxresdefault.jpg"/>
+    <div data-src="https://allnailart.com/wp-content/uploads/2019/05/Almond-acrylic-nails-2019.jpg"/>
+    <div data-src="https://allnailart.com/wp-content/uploads/2019/05/sexy-French-manicure-on-Acrylic-almond-nails.jpg"/>
+    <div data-src="http://img.arttonail.com/full/613f5b4822e74522.jpg"/>
   
   
   </AwesomeSlider>
-);
+
             </div>
         )
     }
