2a8b692b0503fee18d60694cd8a4aad9d4bc8854
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-10-07 14:53:53 -0400

    changed my listofservices

--- client/src/components/ListofServices.js
+++ client/src/components/ListofServices.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-
+import axios from 'axios';
+import { Link} from "react-router-dom"
 class ListofServices extends Component {
     state = {
         services: []
@@ -10,9 +11,12 @@ class ListofServices extends Component {
 
     }
 
-    addService = async () => {
-        const res = await axios.get('/api/services/');
-        this.setState({ services: res.data });
+    addService = () => {
+        axios.get('/api/services/')
+            .then((res) => {
+                // console.log(res)
+                this.setState({ services: res.data });
+            })
 
     };
 
@@ -26,26 +30,26 @@ class ListofServices extends Component {
                 </div>
                 <div class="addbutton">
                     <Link to={`/services/addnew/`} >
-                        <Button >Add New Service</Button>
+                        <button >Add New Service</button>
                     </Link>
-                    </div>
-                    <div class="list">
-                        <ul>
-                            {this.state.services.map(service => (
-                                // <div class="servicels" key={service.id}>
-                                     <li>
-                                    <Link to={`/services/${service.id}`} >
-                                      <label>
-                                            name={service.name}/>
-                                        {/* <div > {service.name}</div> */}
-                                    </Link>
-                                    </li>
-                              
-                            ))
-                              </ul>
-                    
-                    </div>
                 </div>
-                )
-            }
-export default ListofServices;
+                <div class="list">
+                    <ul>
+                        {this.state.services.map(service => (
+                            // <div class="servicels" key={service.id}>
+                            <li>
+                                <Link to={`/services/${service.id}`} >
+                                    {service.name}
+                                </Link>
+                            </li>
+
+                        ))}
+                        
+                        
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+    export default ListofServices;
