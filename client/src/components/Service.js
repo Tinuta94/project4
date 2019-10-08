1c113e56ec2ba493783fe16372ddbc4116722731
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-10-08 12:39:19 -0400

    added delete button and displayed the list of specialist for a certain service

--- client/src/components/Service.js
+++ client/src/components/Service.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios';
-import {Redirect} from "react-router-dom"
+import { Redirect, Link } from "react-router-dom"
+import Specialists from './Specialists';
 
 class Service extends Component {
     state = {
@@ -9,39 +10,61 @@ class Service extends Component {
         options: [],
         redirectToHome: false
     }
-    addService = () => {
-            axios.get(`/api/services/${this.props.match.params.id}/`)
+
+    getServiceFromServer = () => 
+        axios.get(`/api/services/${this.props.match.params.id}/`)
             .then((res) => {
-            this.setState({
-                service: res.data,
-                specialists: res.data.specialists,
-                options: res.data.options})
-        })
-    };
-    
+                //this.state.specialists.filter(specialists => specialists.service === this.service.id)
+                //     this.state.service.filter(service => service.options === this.service.id)
+                this.setState({
+                    service: res.data
+                })
+            })
+
+    getServiceSpecialistsFromServer = () => 
+        axios.get(`/api/specialists/`)
+            .then((res) => {
+                const specialists = res.data.filter(specialist => specialist.service === this.state.service.id)
+                this.setState({ specialists })
+            })
+
+    componentDidMount() {
+        this.getServiceFromServer()
+            .then(this.getServiceSpecialistsFromServer())
+        
+    }
+
 
     handleDeleteService = () => {
         axios.delete(`/api/services/${this.props.match.params.id}/`, this.state.service)
-        .then(() => {
-            this.setState({
-                redirectToHome: true
+            .then(() => {
+                this.setState({
+                    redirectToHome: true
+                })
             })
-        })
+    }
+
+    handleDeleteSpecialist = (specialistId) => {
+        axios.delete(`/api/specialists/${specialistId}/`)
+            .then(() => this.getServiceSpecialistsFromServer())
     }
 
 
     render() {
-        if(this.state.redirectToHome) {
+        if (this.state.redirectToHome) {
             return <Redirect to="/services" />
         }
         return (
             <div>
                 <p>kfklfd</p>
-                  <p>{this.state.service.name}</p>
-                  <button onClick={this.handleDeleteService}>Delete Service</button>
+                <p>{this.state.service.name}</p>
+                <button onClick={this.handleDeleteService}>Delete Service</button>
+                <Link to={`/services/${this.state.service.id}/specialists/new`}>
+                    <button >Add a Specialist</button></Link>
+                {Specialists(this.state.specialists, this.handleDeleteSpecialist)}
             </div>
         )
-        }
     }
+}
 
 export default Service;
