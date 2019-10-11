b138b469864f72806b55fe5fed43fd990417bd17
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-10-11 08:54:37 -0400

    added new text

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,30 +8,44 @@ class Home extends Component {
             <div>
 
                 <div class="firstimage"> </div>
-
-                <p class="primetext">Look Amazing.Feel Beautiful!</p><br></br>
+                 {/* <div class="pic"></div> */}
+                {/* <p class="primetext">Look Amazing &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  Feel Beautiful!</p><br></br> */}
+ 
                 <br></br>
 
                 <div class="container">
 
                     <div class="about">
+                    <p class="mission"> “Beauty comes from the inside. Inside the beauty salon."
+                        </p>
 
-                        <h1 >Un Petit Coin</h1>
+                        <h1 >Un Petit Coin de Paradis</h1>
 
-                        <h1 class="sectext">de Paradis</h1>
-
-                        <p>Un Petit Coin de Paradis Salon, established in 2016 in Atlanta GA,
+                        {/* <h1 class="sectext">de Paradis</h1> */}
+                       
+                       
+                        <p>  Our primary mission is to provide the highest level of customer service to each client, thereby 
+                    establishing a long lasting relationship of trust and commitment.
+                    
+                          Un Petit Coin de Paradis Salon, established in 2016 in Atlanta GA,
                          is owned and operated by professional hairstylists and make-up artists.<br></br>
                             <br></br>
                             Un Petit Coin de Paradis offers an array of color services.Our team is comprised of
                             creative, passionate, professionally trained & licensed stylists.We look forward to helping
                              you look and feel your best!<br></br>
-                            <br></br>
-                            Un Petit Coin de Paradis offers an array of color services.Our team is comprised of
-                            creative, passionate, professionally trained & licensed stylists.We look forward to helping
-                         you look and feel your best! </p>
+                            </p>
                     </div>
                 </div>
+                <div class="nextpic"></div>
+                <div class="newtext">
+                    <p class="new"> Un Petit Coin de Paradis Salon, established in 2016 in Atlanta GA,
+                         is owned and operated by professional hairstylists and make-up artists.<br></br>
+                            Un Petit Coin de Paradis offers an array of color services.Our team is comprised of
+                            creative, passionate, professionally trained & licensed stylists.We look forward to helping
+                             you look and feel your best!<br></br>
+                            </p>
+                </div>
+                <div class="lastpic"></div>
             </div>
 
         )
