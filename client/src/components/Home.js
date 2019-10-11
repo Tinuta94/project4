beec33e333d77e444fee5cd57a8e20a6b75a6cc9
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-10-10 20:11:02 -0400

    made changes to style

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,39 +4,52 @@ import React, { Component } from 'react'
 class Home extends Component {
     render() {
         return (
-            <div>
-                
+           
+                <div>
               
                 <div class="firstimage">
+                    </div>
                 
             
-                 </div>
-     
-                 <h1>Un Petit Coin de Paradis</h1>
                 
-               
+                    <p class="primetext">Look Amazing.Feel Beautiful!</p><br></br>
+                    <br></br>
+                       {/* <p class="secondarytext" > Feel Beautiful!</p> */}
                 <div class="container">
 
                     <div class="about">
-                    <h2>Look Amazing.Feel Beautiful</h2>
+                  
+                    <h1 >Un Petit Coin</h1>
+                    <h1 class="sectext">de Paradis</h1>
+                   
+                   
+                    {/* <div class="pic"></div> */}
                         <p>Un Petit Coin de Paradis Salon, established in 2016 in Atlanta GA,
-                         is owned and operated by professional hairstylists and make-up artists.
+                         is owned and operated by professional hairstylists and make-up artists.<br></br>
+                         <br></br>
                          Un Petit Coin de Paradis offers an array of color services.Our team is comprised of 
-                         creative, passionate, professionally trained & licensed
-                         stylists.We look forward to helping you look and feel your best! Give us a call
-                         to set up an appointment at 770-284-8958, or shoot us an email with any questions
-                     or to set up a consultation.We look forward to helping you look and feel your best! 
-                     Give us a call to set up an appointment at 770-284-8958, 
+                         creative, passionate, professionally trained & licensed stylists.We look forward to helping 
+                         you look and feel your best!<br></br>
+                         <br></br>
+                         Un Petit Coin de Paradis offers an array of color services.Our team is comprised of 
+                         creative, passionate, professionally trained & licensed stylists.We look forward to helping 
+                         you look and feel your best!
+                        
+                         {/* Give us a call
+                         to set up an appointment stylists.We look forward to helping you look and feel your best! at 770-284-8958, or shoot us an email with any questions
+                     or to set up a consultation.We look forward to helping you look and feel your best!  */}
+                     {/* Give us a call to set up an appointment at 770-284-8958, 
                          or shoot us an email with any questions or to set up a consultation.
-                         Give us a call to set up an appointment at 770-284-8958, 
-                         or shoot us an email with any questions or to set up a consultation.
+                         Give us a call to set up an appointment at 770-284-8958,  */}
+                         {/* or shoot us an email with any questions or to set up a consultation. */}
                           </p>
                     </div>
-                    <div class="gif">
-
                     </div>
-                </div>
-            </div>
+                    {/* <div class="gif">
+
+                    </div> */}
+               </div>
+           
         )
     }
 }
