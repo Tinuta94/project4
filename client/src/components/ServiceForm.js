06766fc684f4d07a6d27f9bbd0154cb2e79e286a
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-10-11 08:57:03 -0400

    created new com serviceform

--- client/src/components/ServiceForm.js
+++ client/src/components/ServiceForm.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
-
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import axios from 'axios';
 
  class ServiceForm extends Component {
     state = {
         service: {
             name: "",
+            photo_url: ""
         },
         redirectToHome: false
     }
@@ -36,10 +38,40 @@ render() {
         return <Redirect to="/services" />
     }
     return (
-        <form onSubmit={this.handleSubmit}>
+        <div>
+
+<form onSubmit={this.handleSubmit}>
+                    <TextField
+                        id="outlined-name"
+                        label="Name"
+                        type="text"
+                        name="name"
+                        onChange={this.handleChange}
+                        value={this.state.service.name}
+                        margin="normal"
+                        variant="outlined"
+                    />
+                      <TextField
+        // id="outlined-email-input"
+        label="Image URL"
+        // className={classes.textField}
+        type="img"
+        name="photo_url"
+        onChange={this.handleChange}
+        value={this.state.service.photo_url}
+        autoComplete="url"
+        margin="normal"
+        variant="outlined"
+      />  
+       <Button variant="contained" type="submit" >
+        Submit</Button>
+       </form>
+        {/* <form onSubmit={this.handleSubmit}>
             <input type="text" name="name" onChange={this.handleChange} value={this.state.service.name} />
+            <input type="img" name="photo_url" label="image_url" onChange={this.handleChange} value={this.state.service.photo_url}/>
             <input type="submit" value="Submit" />
-        </form>
+        </form> */}
+        </div>
     )
 }
  }
