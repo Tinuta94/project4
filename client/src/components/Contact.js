0d468ceaf00c633c727a22cd1e10e895e56e00ec
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-10-11 08:55:29 -0400

    added changes to contact

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import { fa_facebook_square } from "react-icons/fa";
+
  class Contact extends Component {
     render() {
         return (
@@ -7,12 +9,17 @@ import React, { Component } from 'react'
             <div class="anim">
            
                 <h3 class="contact">Please contact us to make an appointment or schedule a free consultation with our specialists.</h3>
-
+                <i  class="fab fa-facebook-square"></i>
+                {/* <FontAwesomeIcon icon={faFacebook} /> */}
                  
                 <br></br>           
                   <h3>Email: paradis@gmail.com</h3>
                
                   <h3>Phone: (474)-767-5854</h3>
+                  {/* <span class="fa-stack fa-2x"> */}
+  {/* <i class="fas fa-square fa-stack-2x"></i>
+  <i class="fab fa-twitter fa-stack-1x fa-inverse"></i> */}
+{/* </span> */}
             </div>
             </div>
             
